98f4d9c0e01d4e556b06a29f568f93b4bcc0d45e
Easin Arafat <arafatmrx@gmail.com>
2021-11-11 14:47:33 +0600

    services page

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Navigation from "../../../Shared/Navigation/Navigation";
+import Services from "../Services/Services";
 
 const Home = () => {
   return (
     <div>
       <h1>hello</h1>
       <Navigation></Navigation>
+      <Services></Services>
     </div>
   );
 };
