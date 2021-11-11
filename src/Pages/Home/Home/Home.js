1e648eb5f5cec6b77f4de2c5bba4849c4244eb4d
Easin Arafat <arafatmrx@gmail.com>
2021-11-11 14:10:30 +0600

    app bar  done

---
+++ src/Pages/Home/Home/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navigation from "../../../Shared/Navigation/Navigation";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>hello</h1>
+      <Navigation></Navigation>
+    </div>
+  );
+};
+
+export default Home;
