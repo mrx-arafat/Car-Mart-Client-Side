0c81dfe2d7380462f6a15a44c1eacf4dd34b06af
Easin Arafat <arafatmrx@gmail.com>
2021-11-11 15:31:58 +0600

    home service showed

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,31 +1,24 @@
-import { Container, Grid, Typography } from "@mui/material";
-import { Box } from "@mui/system";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Service from "./Service/Service";
+
+import "./Services.css";
+
 const Services = () => {
+  const [services, setServices] = useState([]);
+  useEffect(() => {
+    fetch("./data.json")
+      .then((res) => res.json())
+      .then((data) => setServices(data));
+  }, []);
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <Container>
-        <Typography
-          sx={{ fontWeight: 500, m: 2, color: "#5CE7ED" }}
-          variant="h6"
-          component="div"
-        >
-          OUR SERVICES
-        </Typography>
-        <Typography sx={{ fontWeight: 600, m: 5 }} variant="h4" component="div">
-          Services We Provide
-        </Typography>
-        <Grid
-          container
-          spacing={{ xs: 2, md: 3 }}
-          columns={{ xs: 4, sm: 8, md: 12 }}
-        >
-          {/* {services.map((service) => (
-              <Service key={service.name} service={service}></Service>
-            ))} */}
-        </Grid>
-      </Container>
-    </Box>
+    <div>
+      <h1 className="p-4 text-danger fw-bold">We Are Offering</h1>
+      <div className="service-container mb-5">
+        {services.map((sv) => (
+          <Service sv={sv}></Service>
+        ))}
+      </div>
+    </div>
   );
 };
 
