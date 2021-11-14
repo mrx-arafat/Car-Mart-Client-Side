46e010ff5a7f72ffb6e3c374bca14f3a4163c37d
Easin Arafat <arafatmrx@gmail.com>
2021-11-14 23:54:42 +0600

    make admin form

--- src/Pages/Dashboard/MakeAdmin/MakeAdmin.js
+++ src/Pages/Dashboard/MakeAdmin/MakeAdmin.js
@@ -1,9 +1,16 @@
+import { Button, TextField } from "@mui/material";
 import React from "react";
 
 const MakeAdmin = () => {
   return (
     <div>
-      <h2>make you admin</h2>
+      <h2>Make an Admin</h2>
+      <form>
+        <TextField sx={{ width: "50%" }} label="Email" variant="standard" />
+        <Button type="submit" variant="contained">
+          Make Admin
+        </Button>
+      </form>
     </div>
   );
 };
